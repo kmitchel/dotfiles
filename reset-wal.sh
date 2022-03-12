6c244639b5dfd6850bcfb6b74bd53bb699bd109c
Kenneth Mitchell <kmitchel46725@gmail.com>
2022-03-12 18:20:41 -0500

    Work on Chrome shortcut.

--- reset-wal.sh
+++ reset-wal.sh
@@ -1,3 +1,3 @@
-wal -c && curl https://source.unsplash.com/random/1920x1080/?water%20mars -L --output ~/Pictures/wal.jpg &> /dev/null && wal -qi ~/Pictures/wal.jpg
+wal -c && curl https://source.unsplash.com/random/1920x1080/?nebula -L --output ~/Pictures/wal.jpg &> /dev/null && wal -qi ~/Pictures/wal.jpg
 cp ~/Pictures/wal.jpg ~/Pictures/$(date +"%s").jpg
 betterlockscreen -u ~/Pictures/wal.jpg &> /dev/null
